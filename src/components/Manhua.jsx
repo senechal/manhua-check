723e31c21a735b3bf1cba5af62a9196cffa351d6
André Luiz Lé Sénéchal Paolino <senechal@users.noreply.github.com>
2024-09-15 19:33:39 +0100

    Add current tab filter. (#26)

--- src/components/Manhua.jsx
+++ src/components/Manhua.jsx
@@ -35,39 +35,51 @@ export const Manhua = (props) => {
     name,
     chapter,
     active,
-    onCounterClick,
-    onSetAcive,
-    onUpdateChapter,
+    source,
+    onUpdate,
     onRemove,
   } = props;
   const { getMessage } = useIntl();
 
   const [edit, setEdit] = useState(false);
   const [loading, setLoading] = useState({
+    source: false,
     counter: false,
     chapter: false,
     active: false,
     remove: false,
   });
   const [chapterInput, setChapterInput] = useState(chapter || "")
+  const [sourceInput, setSourceInput] = useState(source || "")
 
   useEffect(() => {
-    setLoading(false);
+    setLoading((state) => ({ ...state, chapter: false, counter: false }));
     setChapterInput(chapter)
   }, [chapter])
 
   useEffect(() => {
-    setLoading(false);
-  }, [name, active]);
+    setLoading({
+      source: false,
+      counter: false,
+      chapter: false,
+      active: false,
+      remove: false,
+    });
+  }, [name, active, source]);
 
-  const submitHandler = () => {
+  const submitChapterHandler = () => {
     if(active && chapter !== chapterInput && chapterInput !== "") {
       setLoading((state) => ({ ...state, chapter: true }));
-      onUpdateChapter(chapterInput)
+      onUpdate('chapter', parseInt(chapterInput, 10))
     }
   }
 
-  const handleEnter = (e) => {
+  const submitSourceHandler = () => {
+    setLoading((state) => ({ ...state, source: true }));
+    onUpdate('source', sourceInput)
+  }
+
+  const handleEnter = (submitHandler) => (e) => {
     if (e.keyCode === 13 || e.which === 13) {
       submitHandler()
     }
@@ -82,15 +94,15 @@ export const Manhua = (props) => {
             <Edit className='size-5 text-white cursor-pointer hover:text-[#e1e243]' onClick={() => setEdit(state => !state)} />
           </div>
           <p className="text-[#e1e243] cursor-default text-sm font-normal leading-normal line-clamp-2">{getMessage('CHAPTER')} {active ? chapter : '?'}</p>
-
-
         </div>
         <div className="shrink-0">
           <button
             className="flex min-w-[32px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-8 px-2 bg-[#153645] hover:bg-[#2d7393] text-white text-sm font-medium leading-normal w-fit"
-            onClick={(e) => {
+            onClick={() => {
               setLoading((state) => ({ ...state, counter: true }));
-              onCounterClick(e);
+              if(active) {
+                onUpdate('chapter', parseInt(chapter, 10) + 1)
+              }
             }}
           >
             <span className="truncate">
@@ -108,6 +120,7 @@ export const Manhua = (props) => {
         edit ? (
           <div className='flex flex-col gap-4 px-4 justify-end'>
             <hr className='border-gray-800 dark:border-white' />
+            {/** CHAPTER INPUT */}
             <div className="flex items-center justify-end">
               <label className="flex flex-col min-w-40 flex-1 pr-4">
                 <input
@@ -115,13 +128,13 @@ export const Manhua = (props) => {
                   className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-white focus:outline-0 focus:ring-0 border-none bg-[#153645] focus:border-none h-10 placeholder:text-white p-4 text-base font-normal leading-normal"
                   value={chapterInput}
                   onChange={({ target }) => setChapterInput((state) => validateNumber(target.value, state))}
-                  onKeyDown={handleEnter}
+                  onKeyDown={handleEnter(submitChapterHandler)}
                 />
               </label>
               <button
                 disabled={chapter === chapterInput || chapterInput === ""}
                 className="disabled:bg-slate-200 disabled:cursor-not-allowed flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-14 px-4 bg-[#e1e243] hover:bg-[#a5a51a] text-[#0f1a24] gap-2 pl-4 text-sm font-bold leading-normal tracking-[0.015em]"
-                onClick={submitHandler}
+                onClick={submitChapterHandler}
               >
                 {
                   loading.chapter
@@ -131,12 +144,38 @@ export const Manhua = (props) => {
 
               </button>
             </div>
+            {/** SOURCE INPUT */}
+            <div className="flex items-center justify-end">
+              <label className="flex flex-col min-w-40 flex-1 pr-4">
+                <input
+                  placeholder={getMessage('SOURCE')}
+                  className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-white focus:outline-0 focus:ring-0 border-none bg-[#153645] focus:border-none h-10 placeholder:text-white p-4 text-base font-normal leading-normal"
+                  value={sourceInput}
+                  onChange={({ target }) => setSourceInput(target.value)}
+                  onKeyDown={handleEnter(submitSourceHandler)}
+                />
+              </label>
+              <button
+                disabled={source === sourceInput}
+                className="disabled:bg-slate-200 disabled:cursor-not-allowed flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-14 px-4 bg-[#e1e243] hover:bg-[#a5a51a] text-[#0f1a24] gap-2 pl-4 text-sm font-bold leading-normal tracking-[0.015em]"
+                onClick={submitSourceHandler}
+              >
+                {
+                  loading.source
+                    ? <Spinner className='animate-spin size-6 text-[#0f1a24] cursor-pointer' />
+                    : <Enter className='size-6 text-[#0f1a24] cursor-pointer' />
+                }
+
+              </button>
+            </div>
+            {/** ACTIVE TOGGLE */}
             <p className="flex items-center text-white text-sm font-normal leading-normal line-clamp-2">
-              {getMessage('ACTIVE')} <Active active={active} loading={loading.active} onClick={(e) => {
+              {getMessage('ACTIVE')} <Active active={active} loading={loading.active} onClick={() => {
                 setLoading((state) => ({ ...state, active: true }));
-                onSetAcive(e);
+                onUpdate('active', !active)
               }} />
             </p>
+            {/** REMOVE BUTTON */}
             <p className="flex items-center text-white text-sm font-normal leading-normal line-clamp-2">
               {getMessage('REMOVE')}
               {
